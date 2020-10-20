f683973cf52b7c5aaa4bcacde93ba1e45f80c3fd
Mónica <serracin.monica@gmail.com>
2020-10-20 09:39:37 -0600

    NotificacionDTO y services

---
+++ src/main/java/org/una/unaeropuertoclient/model/NotificacionDto.java
@@ -0,0 +1,11 @@
+package org.una.unaeropuertoclient.model;
+
+import java.util.Date;
+
+public class NotificacionDto {
+    int id;
+    boolean activo;
+    int idObjeto;
+    Date fechaRegistro;
+    AreaDto areasId;
+}
