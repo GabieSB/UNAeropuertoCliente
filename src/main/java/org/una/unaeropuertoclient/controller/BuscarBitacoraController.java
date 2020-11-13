020700dac0b14fb1465a79c33cdc2fce717f5a45
Roberth <roberth123pz@gmail.com>
2020-11-12 22:41:11 -0600

    Se habilitó consulta de bitácoras.

--- src/main/java/org/una/unaeropuertoclient/controller/BuscarBitacoraController.java
+++ src/main/java/org/una/unaeropuertoclient/controller/BuscarBitacoraController.java
@@ -13,13 +13,14 @@ import javafx.scene.layout.VBox;
 import org.una.unaeropuertoclient.model.BitacoraDto;
 import org.una.unaeropuertoclient.service.BitacoraService;
 import org.una.unaeropuertoclient.utils.*;
-
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 
-public class BuscarBitacoraController  extends Controller implements Initializable {
+public class BuscarBitacoraController extends Controller implements Initializable {
+
     public DatePicker dateInicio;
     public DatePicker dateFin;
     public JFXButton btnBuscarFecha;
@@ -33,8 +34,6 @@ public class BuscarBitacoraController  extends Controller implements Initializab
     public TableView tableResultados;
     public VBox containerRoot;
 
-    List<BitacoraDto> bitacorasResultado = new ArrayList<>();
-    
     BitacoraService bitacoraService = new BitacoraService();
 
     @Override
@@ -42,17 +41,13 @@ public class BuscarBitacoraController  extends Controller implements Initializab
         iniciarComponentes();
     }
 
-    public void iniciarComponentes(){
+    public void iniciarComponentes() {
         ObservableList<String> items = FXCollections.observableArrayList();
         FlowController.changeSuperiorTittle("BITÁCORAS");
         FlowController.changeCodeScreenTittle("BG000");
-
-
-        items.addAll("ID", "CÉDULA USUARIO", "ACCIÓN");
+        items.addAll("ID", "CÉDULA USUARIO");
         comboxParametros.getItems().addAll(items);
-
         cargarPropiedadesTable();
-
     }
 
     private void cargarPropiedadesTable() {
@@ -63,45 +58,45 @@ public class BuscarBitacoraController  extends Controller implements Initializab
         columUsuario.setCellValueFactory(x -> new SimpleStringProperty(x.getValue().getUsuariosId().getNombre()));
     }
 
-    public void busquedaSegunCategoria(){
+    @SuppressWarnings("null")
+    public void busquedaSegunCategoria() {
         int categoria = comboxParametros.getSelectionModel().getSelectedIndex();
         String valor = txtValorBuscado.getText();
-        
-        Respuesta respuesta = null;
 
-        if(!valor.isEmpty()){
-            if(categoria == 0){
-                if(Validar.isLongNumber(valor)){
-                    respuesta = bitacoraService.buscarPorID(Long.parseLong(valor));
-                }else  new Mensaje().show(Alert.AlertType.INFORMATION, "Información", "Parece que has ingresado un valor incorrecto. El ID debería ser un número");
-
-            }else if(categoria == 1){
-                respuesta= bitacoraService.buscarPorUserCedula(valor);
-            }else if(categoria == 2){
-                respuesta = bitacoraService.buscarPorTipoCambio(valor);
+        if (!valor.isEmpty()) {
+            Respuesta respuesta = null;
+            switch (categoria) {
+                case 0:
+                    if (Validar.isLongNumber(valor)) {
+                        respuesta = bitacoraService.buscarPorID(Long.parseLong(valor));
+                    } else {
+                        new Mensaje().show(Alert.AlertType.INFORMATION, "Información", "Parece que has ingresado un valor incorrecto. El ID debería ser un número");
+                    }
+                    break;
+                case 1:
+                    respuesta = bitacoraService.buscarPorUserCedula(valor);
+                    break;
+                case 2:
+                    respuesta = bitacoraService.buscarPorTipoCambio(valor);
+                    break;
+                default:
+                    Platform.runLater(() -> new Mensaje().show(Alert.AlertType.WARNING, "Atención", "Selecciona un tipo de búsqueda."));
+                    break;
             }
-
-            if(respuesta != null && respuesta.getEstado()){
-                if(categoria == 0) bitacorasResultado.add((BitacoraDto) respuesta.getResultado("data"));
-                else bitacorasResultado = (List<BitacoraDto>) respuesta.getResultado("data");
-
-                if(bitacorasResultado.size() == 0) new Mensaje().show(Alert.AlertType.INFORMATION, "Informacion", "No hubo resulatos");
-                else llenarTabla();
-
-            }else{
-                new Mensaje().showModal(Alert.AlertType.ERROR, "Falla al realizar la búsqueda", this.getStage(), "No se pudieron obtener resultados");
-
+            tableResultados.getItems().clear();
+            if (respuesta != null && respuesta.getEstado()) {
+                if (categoria == 0) {
+                    tableResultados.getItems().add((BitacoraDto) respuesta.getResultado("data"));
+                } else {
+                    tableResultados.getItems().addAll((List<BitacoraDto>) respuesta.getResultado("data"));
+                }
+            } else {
+                new Mensaje().showModal(Alert.AlertType.ERROR, "Atención", this.getStage(), respuesta.getMensaje());
             }
-        }else new Mensaje().showModal(Alert.AlertType.WARNING, "Información", this.getStage(), "Parece que el espacio de valor a consultar esta vacío");
-
-
+        } else {
+            new Mensaje().showModal(Alert.AlertType.WARNING, "Información", this.getStage(), "Parece que el espacio de valor a consultar esta vacío");
+        }
     }
-
-    public void llenarTabla(){
-        tableResultados.getItems().clear();
-        tableResultados.setItems(FXCollections.observableList(bitacorasResultado));
-    }
-
 
     @Override
     public void initialize() {
@@ -109,15 +104,21 @@ public class BuscarBitacoraController  extends Controller implements Initializab
 
     }
 
+    @FXML
     public void buscarPorFecha(ActionEvent actionEvent) {
-
-        Respuesta  respuesta = bitacoraService.buscarEntreFechas(dateInicio.getValue(), dateFin.getValue());
-        if(respuesta.getEstado()){
-            bitacorasResultado = (List<BitacoraDto>) respuesta.getResultado("data");
-            llenarTabla();
-        }else{
-
-        }
+        ButtonWaitUtils.aModoEspera(btnBuscarFecha);
+        Thread th = new Thread(() -> {
+            Respuesta respuesta = bitacoraService.buscarEntreFechas(dateInicio.getValue(), dateFin.getValue());
+            Platform.runLater(() -> {
+                ButtonWaitUtils.salirModoEspera(btnBuscarFecha, "Buscar por fechas");
+                if (respuesta.getEstado()) {
+                    tableResultados.getItems().addAll((List)respuesta.getResultado("data"));
+                } else {
+                    new Mensaje().showModal(Alert.AlertType.WARNING, "Atención", this.getStage(), respuesta.getMensaje());
+                }
+            });
+        });
+        th.start();
     }
 
     public void buscarPorParametro(ActionEvent actionEvent) {
